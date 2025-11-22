d5ccca3480971564a19e97ae00715c6c3690f792
Sontref <sosontref@gmail.com>
2025-11-22 14:50:08 +0000

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,20 +1,27 @@
 dependencies = ['torch']
 import os
-import sys
 import torch
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
-from silero_stress.accentor import load_accentor
 
 
-def silero_stress():
+def silero_stress(lang='ru'):
     """Silero Stress: automated stress and homograph disambiguation
     Returns a package with pre-built utils
     Please see https://github.com/snakers4/silero-stress for usage examples
-    """
 
-    package_path = os.path.join(os.path.dirname(__file__), 'src', 'silero_stress', 'data', 'accentor.pt')
+    Params  
+    lang: str - accentor language ('ru' or 'ukr')
+    """
+    if lang not in ['ru', 'ukr']:
+        print(f'Wrong language {lang}. Must be in ["ru", "ukr"].')
+    model_name = 'accentor.pt' if lang == 'ru' else 'accentor-ukr.pt'
+    package_path = os.path.join(os.path.dirname(__file__), 'src', 'silero_stress', 'data', model_name)
     accentor = torch.package.PackageImporter(package_path).load_pickle("accentor_models", "accentor")
-    quantized_weight = accentor.homosolver.model.bert.embeddings.word_embeddings.weight.data.clone()
-    restored_weights = accentor.homosolver.model.bert.scale * (quantized_weight - accentor.homosolver.model.bert.zero_point)
-    accentor.homosolver.model.bert.embeddings.word_embeddings.weight.data = restored_weights
+    if lang == 'ru':
+        quantized_weight = accentor.homosolver.model.bert.embeddings.word_embeddings.weight.data.clone()
+        restored_weights = accentor.homosolver.model.bert.scale * (quantized_weight - accentor.homosolver.model.bert.zero_point)
+        accentor.homosolver.model.bert.embeddings.word_embeddings.weight.data = restored_weights
+    else:
+        quantized_weight = accentor.accentor.model.embedding.weight.data.clone()
+        restored_weights = accentor.accentor.model.scale * (quantized_weight - accentor.accentor.model.zero_point)
+        accentor.accentor.model.embedding.weight.data = restored_weights
     return accentor
